7cd4330ae825504b7d8ec27f646de7b5bd912d20
Antonio Roa <antu_8889@hotmail.com>
2019-03-19 12:36:42 -0600

    Archivo de evidencia de mi curso

---
+++ Pruebas_AntonioRoa.php
@@ -0,0 +1,17 @@
+<?php
+// pruebas de Antonio Roa en el uso de php
+$a=5;
+
+switch($a){
+case 1:
+case 2:
+echo 'funciono';
+break;
+case 3:
+echo 'no funciono';
+break;
+default:
+echo 'salio bien';
+break;
+}
+?>
